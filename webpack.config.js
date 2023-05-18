1c919cf33fb56ed370bbe5059aec0296f694bbbe
rgilks <rob.gilks@gmail.com>
2023-05-18 10:04:23 +0100

    Remove testing while getting build to run

--- webpack.config.js
+++ webpack.config.js
@@ -40,42 +40,11 @@ const appConfig = {
       template: './public/index.html.tmpl',
       filename: 'index.html',
       templateParameters: {
-        basePath: process.env.BASE_PATH ?? 'http://localhost:3001/'
+        basePath: process.env.BASE_PATH ?? 'http://localhost:3000/'
       }
     }),
     new HtmlBeautifierPlugin()
   ]
 }
 
-const testConfig = {
-  mode: 'development',
-  entry: './browser-test/index.js',
-  output: {
-    path: path.resolve(__dirname, 'browser-test'),
-    filename: 'js/libs/bundle.js',
-    clean: false
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-        resolve: {
-          fullySpecified: false,
-          alias: {
-            models: '../src/amplify/models/index.js',
-            'aws-exports': '../src/amplify/aws-exports.js',
-            js: '../src/js'
-          }
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
-}
-
-module.exports = [appConfig, testConfig]
+module.exports = [appConfig]
