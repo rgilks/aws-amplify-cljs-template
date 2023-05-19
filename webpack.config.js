f54d6b6dc0e5c39c649fa35f492cdd07644e9c5c
rgilks <rob.gilks@gmail.com>
2023-05-19 08:01:06 +0100

    Set up unit testing

--- webpack.config.js
+++ webpack.config.js
@@ -47,4 +47,35 @@ const appConfig = {
   ]
 }
 
-module.exports = [appConfig]
+const testConfig = {
+  mode: 'development',
+  entry: './browser-test/index.js',
+  output: {
+    path: path.resolve(__dirname, 'browser-test'),
+    filename: 'js/libs/bundle.js',
+    clean: false
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+        resolve: {
+          fullySpecified: false,
+          alias: {
+            models: '../src/amplify/models/index.js',
+            'aws-exports': '../src/amplify/aws-exports.js',
+            js: '../src/js'
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
+
+module.exports = [appConfig, testConfig]
