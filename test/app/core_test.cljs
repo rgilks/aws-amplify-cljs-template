ae7c6e485b4eef9e33d125c8e9058c41e1136c60
rgilks <rob.gilks@gmail.com>
2023-05-23 22:13:58 +0100

    Improved test

--- test/app/core_test.cljs
+++ test/app/core_test.cljs
@@ -8,8 +8,15 @@
 (deftest init-hub-listeners-test
   (let [event-dispatched (atom nil)]
     (with-redefs [refx/dispatch #(reset! event-dispatched %)]
-      (core/init-hub-listeners! 
+      (core/init-hub-listeners!
        [["test-channel" "test-event-1" ::test-event-1]])
+      (is (nil? @event-dispatched))
+      (-> amplify/Hub
+          (.dispatch "test-channel" (clj->js {:event "X"})))
+      (is (nil? @event-dispatched))
+      (-> amplify/Hub
+          (.dispatch "X" (clj->js {:event "test-event-1"})))
+      (is (nil? @event-dispatched))
       (-> amplify/Hub
           (.dispatch "test-channel" (clj->js {:event "test-event-1"})))
       (is (= @event-dispatched [::test-event-1])))))
